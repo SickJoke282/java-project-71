c460c01c87b6e84766aa4e05234c182ab9ab0ad0
Yegor <egorkrylatov18@gmail.com>
2023-06-27 12:12:34 +0500

    rename differ to gendiff

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,7 +3,7 @@ import picocli.CommandLine;
 import picocli.CommandLine.Command;
 
 
-@Command(name = "differ", mixinStandardHelpOptions = true, version = "checksum 4.7.4",
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "checksum 4.7.4",
         description = "Compares two configuration files and shows a difference.")
 class App {
     public static void main(String... args) {
