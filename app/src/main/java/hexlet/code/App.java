092674598113e24765aa44c1b08d70a908464db0
Yegor <egorkrylatov18@gmail.com>
2023-06-27 14:46:22 +0500

    add the call of generate method of Differ static class

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -5,22 +5,26 @@ import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.Callable;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "checksum 4.7.4",
         description = "Compares two configuration files and shows a difference.")
 class App implements Callable<Integer>{
     @Parameters(index = "0", paramLabel = "filepath1", description = "path to the first file")
-    private String filepath1;
+    private File file1;
 
     @Parameters(index = "1", paramLabel = "filepath2", description = "path to the second file")
-    private String filepath2;
+    private File file2;
 
     @Option(names = {"-f", "--format"}, paramLabel = "format", description = "output format [default: stylish]")
     private String format = "stylish";
 
     @Override
-    public Integer call() {
+    public Integer call() throws Exception {
+        String diff = Differ.generate(file1, file2);
+        System.out.println(diff);
         return 0;
     }
     public static void main(String... args) {
