afe3362600e0acccdb15712d62ce7ddbae21c2ec
Yegor <egorkrylatov18@gmail.com>
2023-07-03 12:09:54 +0500

    add a checker for a format

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -6,13 +6,20 @@ import java.util.Map;
 
 
 public class Differ {
-    public static String generate(File file1, File file2) throws IOException {
-        String result = "{\n";
+    public static String generate(File file1, File file2, String formatter) throws IOException {
         Map<String, Object>[] parsedFiles = Parser.parse(file1, file2);
         Map<String, Object> container1 = parsedFiles[0];
         Map<String, Object> container2 = parsedFiles[1];
+        if (formatter.equals("stylish")) {
+            return stylishGenerate(container1, container2);
+        }
+        return null;
+    }
+    public static String stylishGenerate(Map<String, Object> container1, Map<String, Object> container2) {
+        String result = "{\n";
         for (Map.Entry<String, Object> entryOfContainer1: container1.entrySet()) {
             if (container2.containsKey(entryOfContainer1.getKey())) {
+                System.out.println(entryOfContainer1.getValue());
                 if (container2.get(entryOfContainer1.getKey()).equals(entryOfContainer1.getValue())) {
                     result += "   " + entryOfContainer1.getKey() + ": " + entryOfContainer1.getValue() + "\n";
                     container2.remove(entryOfContainer1.getKey());
