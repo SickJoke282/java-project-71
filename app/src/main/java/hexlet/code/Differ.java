12f2d0e1da3a27f37f37988d186e0bb7efb94d65
Yegor <egorkrylatov18@gmail.com>
2023-07-03 20:01:22 +0500

    add a new logic with multimap

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,5 +1,8 @@
 package hexlet.code;
 
+import com.google.common.collect.LinkedHashMultimap;
+import com.google.common.collect.Multimap;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.Map;
@@ -10,34 +13,35 @@ public class Differ {
         Map<String, Object>[] parsedFiles = Parser.parse(file1, file2);
         Map<String, Object> container1 = parsedFiles[0];
         Map<String, Object> container2 = parsedFiles[1];
-        if (formatter.equals("stylish")) {
-            return stylishGenerate(container1, container2);
-        }
-        return null;
+        Multimap<String, Map.Entry<String, Object>> answer = trackChangesOfFiles(container1, container2);
+        return switch (formatter) {
+            case "stylish" -> Formatter.stylishGenerate(answer);
+            case "plain" -> Formatter.plainGenerate(answer);
+            default -> null;
+        };
     }
-    public static String stylishGenerate(Map<String, Object> container1, Map<String, Object> container2) {
-        String result = "{\n";
+    public static Multimap<String, Map.Entry<String, Object>> trackChangesOfFiles(Map<String,
+            Object> container1, Map<String, Object> container2) {
+        Multimap<String, Map.Entry<String, Object>> answer = LinkedHashMultimap.create();
         for (Map.Entry<String, Object> entryOfContainer1: container1.entrySet()) {
             if (container2.containsKey(entryOfContainer1.getKey())) {
-                System.out.println(entryOfContainer1.getValue());
                 if (container2.get(entryOfContainer1.getKey()).equals(entryOfContainer1.getValue())) {
-                    result += "   " + entryOfContainer1.getKey() + ": " + entryOfContainer1.getValue() + "\n";
+                    answer.put("    ", Map.entry(entryOfContainer1.getKey(), entryOfContainer1.getValue()));
                     container2.remove(entryOfContainer1.getKey());
                 } else {
-                    result += " - " + entryOfContainer1.getKey() + ": " + entryOfContainer1.getValue() + "\n";
-                    result += " + " + entryOfContainer1.getKey()
-                            + ": " + container2.get(entryOfContainer1.getKey())
-                            + "\n";
+                    answer.put("  - ",
+                            Map.entry(entryOfContainer1.getKey(), entryOfContainer1.getValue()));
+                    answer.put("  + ",
+                            Map.entry(entryOfContainer1.getKey(), container2.get(entryOfContainer1.getKey())));
                     container2.remove(entryOfContainer1.getKey());
                 }
             } else {
-                result += " - " + entryOfContainer1.getKey() + ": " + entryOfContainer1.getValue() + "\n";
+                answer.put("  - ", Map.entry(entryOfContainer1.getKey(), entryOfContainer1.getValue()));
             }
         }
         for (Map.Entry<String, Object> entryOfContainer2: container2.entrySet()) {
-            result += " + " + entryOfContainer2.getKey() + ": " + entryOfContainer2.getValue() + "\n";
+            answer.put("  + ", Map.entry(entryOfContainer2.getKey(), entryOfContainer2.getValue()));
         }
-        result += "}";
-        return result;
+        return answer;
     }
 }
