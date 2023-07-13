cc851e5a55bd09e207f20be6b0b6d666889676eb
Yegor <egorkrylatov18@gmail.com>
2023-07-13 12:39:36 +0500

    fix

--- app/src/main/java/hexlet/code/Tree.java
+++ app/src/main/java/hexlet/code/Tree.java
@@ -15,30 +15,31 @@ public class Tree {
         SortedSet<String> sortedSet = new TreeSet<>(container1.keySet());
         sortedSet.addAll(container2.keySet());
         for (String key: sortedSet) {
+            Object val1 = (container1.get(key) == null ? "null" : container1.get(key));
+            Object val2 = (container2.get(key) == null ? "null" : container2.get(key));
             if (!container2.containsKey(key)) {
                 Map<String, Object> node = Map.of(
                         "type", "removed",
-                        key, (container1.get(key) == null ? "null" : container1.get(key))
+                        key, val1
                 );
                 answer.add(node);
             } else if (!container1.containsKey(key)) {
                 Map<String, Object> node = Map.of(
                         "type", "added",
-                        key, (container2.get(key) == null ? "null" : container2.get(key))
+                        key, val2
                 );
                 answer.add(node);
-            } else if (!(container1.get(key) == null ? "null" : container1.get(key))
-                    .equals((container2.get(key) == null ? "null" : container2.get(key)))) {
+            } else if (!val1.equals(val2)) {
                 Map<String, Object> node = Map.of(
                         "type", "changed",
-                        key, (container1.get(key) == null ? "null" : container1.get(key)),
-                        "value2", (container2.get(key) == null ? "null" : container2.get(key))
+                        key, val1,
+                        "value2", val2
                 );
                 answer.add(node);
             } else {
                 Map<String, Object> node = Map.of(
                         "type", "unchanged",
-                        key, (container1.get(key) == null ? "null" : container1.get(key))
+                        key, val1
                 );
                 answer.add(node);
             }
--- app/src/main/java/hexlet/code/formatters/PlainFormatter.java
+++ app/src/main/java/hexlet/code/formatters/PlainFormatter.java
@@ -13,18 +13,10 @@ public class PlainFormatter {
             String key = map.keySet().stream()
                     .filter(x -> !x.equals("type") && !x.equals("value2"))
                     .collect(Collectors.joining());
-            Object adaptiveValueToRead1 = (map.get(key).getClass() == LinkedHashMap.class
-                    || map.get(key).getClass() == ArrayList.class)
-                    ? "[complex value]" : (map.get(key).getClass() == String.class
-                    && !map.get(key).equals("null"))
-                    ? "'" + map.get(key) + "'" : map.get(key);
+            Object adaptiveValueToRead1 = giveAdaptiveOutput(map, key);
             Object adaptiveValueToRead2 = null;
             if (map.containsKey("value2")) {
-                adaptiveValueToRead2 = (map.get("value2").getClass() == LinkedHashMap.class
-                        || map.get("value2").getClass() == ArrayList.class)
-                        ? "[complex value]" : (map.get("value2").getClass() == String.class
-                        && !map.get("value2").equals("null"))
-                        ? "'" + map.get("value2") + "'" : map.get("value2");
+                adaptiveValueToRead2 = giveAdaptiveOutput(map, "value2");
             }
             if (map.get("type").equals("removed")) {
                 result = result.concat("Property '" + key + "' was removed\n");
@@ -37,5 +29,12 @@ public class PlainFormatter {
             }
         }
         return result.substring(0, result.lastIndexOf('\n'));
+    }
+    public static Object giveAdaptiveOutput(Map<String, Object> map, String key) {
+        return (map.get(key).getClass() == LinkedHashMap.class
+                || map.get(key).getClass() == ArrayList.class)
+                ? "[complex value]" : (map.get(key).getClass() == String.class
+                && !map.get(key).equals("null"))
+                ? "'" + map.get(key) + "'" : map.get(key);
     }
 }
